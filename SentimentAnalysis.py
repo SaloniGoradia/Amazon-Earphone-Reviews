b64d9ff74cdc426cbcf8b70c5431a3b1356e6547
Saloni Goradia <goradia.saloni@gmail.com>
2020-09-04 21:18:05 -0700

    nltk_data

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -9,7 +9,7 @@ import plotly as ply
 import plotly.express as px
 import streamlit as st
 import nltk
-nltk.data.path.append('./nltk_data/')
+nltk.data.path.append('C:/Users/sgoradia/Desktop/Python/SentimentAnalysis/nltk_data')
 nltk.downloader.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 sia = SentimentIntensityAnalyzer()
